8acdf5f874628d9e695915c76071cd87eca7a632
1204-raj <200601204@student.georgianc.on.ca>
2025-12-02 12:56:13 -0500

    Update terrific_crift1.ino

--- terrific_crift1.ino
+++ terrific_crift1.ino
@@ -1,14 +1,17 @@
-// C++ code
-//
-void setup()
-{
-  pinMode(LED_BUILTIN, OUTPUT);
+int leds[6] = {2, 3, 4, 5, 6, 7};   // LED pins
+int delayTime = 400;                // Custom delay (NOT same as class demo)
+
+void setup() {
+  for (int i = 0; i < 6; i++) {
+    pinMode(leds[i], OUTPUT);
+  }
 }
 
-void loop()
-{
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000); // Wait for 1000 millisecond(s)
+void loop() {
+  // Turn LEDs on one by one
+  for (int i = 0; i < 6; i++) {
+    digitalWrite(leds[i], HIGH);
+    delay(delayTime);
+    digitalWrite(leds[i], LOW);
+  }
 }
